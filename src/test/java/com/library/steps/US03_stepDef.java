8f3caa9dac6f8aaf957ecf217288caa2b3082d13
nail-92 <nail-92@live.com>
2023-08-13 12:41:04 -0500

    US03 done

---
+++ src/test/java/com/library/steps/US03_stepDef.java
@@ -0,0 +1,30 @@
+package com.library.steps;
+
+import com.library.pages.BookPage;
+import com.library.utility.BrowserUtil;
+import com.library.utility.DB_Util;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import org.junit.Assert;
+
+import java.util.List;
+
+public class US03_stepDef {
+    BookPage bookPage = new BookPage();
+    @And("the user clicks book categories")
+    public void theUserClicksBookCategories() {
+        bookPage.mainCategoryElement.click();
+    }
+
+    @Then("verify book categories must match book_categories table from db")
+    public void verifyBookCategoriesMustMatchBook_categoriesTableFromDb() {
+        List<String> actual_categories = BrowserUtil.getAllSelectOptions(bookPage.mainCategoryElement);
+        actual_categories.remove(0);
+        String query = "select name from book_categories;";
+        DB_Util.runQuery(query);
+        List<String> expected_categories = DB_Util.getColumnDataAsList(1);
+        Assert.assertEquals(actual_categories,expected_categories);
+
+
+    }
+}
